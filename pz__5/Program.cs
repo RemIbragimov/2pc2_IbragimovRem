5b5bf7a62c62be28ece0daa9f7c01ad05131e614
1073 <1073@LAPTOP-23N9FH46>
2021-09-28 13:01:32 +0300

    Добавьте файлы проекта.

---
+++ pz__5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pz__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            for (int i = 30; i <= 120; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Задание 2");
+            for (int i = 0; i < 5; i += 1)
+            {
+                Console.Write($"{(char)(i + 'M')}\t");
+            }
+            Console.WriteLine("Задание 3");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(new string('#', 4));
+            }
+            Console.WriteLine("Задание 4");
+            for (int i = 0; i <= 100; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Задание 5");
+
+            for (int i = 4, j = 50; Math.Abs(i - j) != 12; i++, j--)
+            {
+                Console.WriteLine("{0} {1}", i, j);
+            }
+        }
+    }
+}
