378eb2a88ad889b4d6aa12c6d790d5771ac236b8
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-09-29 12:58:59 +0300

    1

---
+++ pz_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            while (n >= 2)
+            {
+                k++;
+                n /= 2;
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
