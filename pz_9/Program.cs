7a788ecca4cd18993c7b51844ef9f1fb9b62a27a
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-10-15 13:06:44 +0300

    pz_9

---
+++ pz_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int sum = 0;
+            int[] S = new int[4];
+            int[,] A = new int[3, 4];
+            Console.WriteLine("Массив А: ");
+            for (i = 0; i <= 2; i++)
+            {
+                for (j = 0; j <= 3; j++)
+
+                {
+                    A[i, j] = i + 4 * j + 1;
+                    Console.Write(A[i, j] + " ");
+
+                    sum += A[i, j];
+                }
+                Console.WriteLine("=" + " " + sum);
+                for (i = 0; i < 4; i++)
+                {
+                    for (j = 0; j < 3; j++)
+                    {
+                        S[i] += A[j, i];
+                    }
+                }
+
+            }
+        }
+    }
+}
