a2a3892ac1e1580ab4eabf7890c2a8684372ee04
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-11 20:23:36 +0300

    pz_12

---
+++ pz_12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = new string[4];
+            str[0] = "Катя ещё покорит каждого";
+            str[1] = "Ты самая лучшая из всех!";
+            str[2] = "Как дела?";
+            str[3] = "Пока";
+            
+
+            var palka = str.OrderBy(a => a.Length);
+
+            foreach (string item in palka)
+                Console.WriteLine(item);
+        }
+    }
+}
