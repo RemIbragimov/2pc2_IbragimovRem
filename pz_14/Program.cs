15f16059de737d94eb22b3d5655ce41c600b3b89
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-23 20:43:12 +0300

    pz_14

---
+++ pz_14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace pz_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string[] mass = File.ReadAllLines(@"C:\pz\14.txt");
+            StreamWriter NewFile = File.CreateText(@"F:\pz\123.txt");
+            for (a = 0; a < mass.Length; a++)
+            {
+                if (mass[a] != "")
+                {
+                    NewFile.WriteLine(mass[a]);
+                }
+            }
+            NewFile.Close();
+        }
+    }
+}
