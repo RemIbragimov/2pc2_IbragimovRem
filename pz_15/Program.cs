4f4f436fca1adcd1b8e03f6d37496dc66727ea16
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-24 22:04:22 +0300

    pz_15

---
+++ pz_15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\1073\Desktop";
+            string subpath = @"new";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists) 
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+
+
+            var myFile = File.Create(@"C:\Users\1073\Desktop\new\f2.txt"); 
+
+            myFile.Close();
+
+            File.AppendAllText(@"C:\Users\1073\Desktop\new\f2.txt", File.ReadAllText(@"C:\Users\1073\Desktop\f1.txt")); 
+
+        }
+    }
+}
